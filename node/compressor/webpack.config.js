89484dde65d344e853c6abe67fd1517ac9195335
comlewod <comlewod@163.com>
2016-03-16 23:58:39 +0800

    change webpack.config.js

--- node/compressor/webpack.config.js
+++ node/compressor/webpack.config.js
@@ -1,11 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
 	entry: './../../pages/tests/main.js',
+	//entry: path.resolve(__dirname, './main.js'),
+	//entry: './main.js',
 	output: {
 		path: './../../output',
 		filename: 'final.js'
 	},
 	module: {
-		test: /\.css$/,
-		loader: 'style!css'
-	}
+		loaders: [
+			{ 
+				test: /\.css$/, 
+				loader: "style-loader!css-loader",
+			}
+		]
+	},
+	resolve: {
+		root: [
+			path.resolve('/')
+		],
+		//moduleDirectories: [path.resolve(__dirname)],
+		extensions: ['', '.js', '.css']
+	},
 };
