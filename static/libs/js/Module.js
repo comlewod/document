75bee01670b97fb4378009b6b6892c389644603b
comlewod <comlewod@163.com>
2016-05-31 21:31:05 +0800

    finished module

--- static/libs/js/Module.js
+++ static/libs/js/Module.js
@@ -2,21 +2,35 @@ var G = G || {};
 
 (function(){
 	var module = {};
+	module.fn = {};
 
 	module.define = function(name, fn){
 		if( typeof fn != 'object' ) return;
+		module.fn[name] = fn;
 	};
 	module.use = function(name, opts){
+		module.fn[name].init(opts);
+	};
+	module.require = function(name, opts){
+		var now_fn = module.fn[name];
+		return now_fn;
 	};
 
 	G.define = module.define;
 	G.use = module.use;
+	G.require = module.require;
 })();
 
 G.define('post/page', {
-	bindEvent: function(opts){
-		console.log(opts.name);
-	});
+	init: function(opts){
+		this.bindEvent();
+	},
+	bindEvent: function(){
+		console.log(23424);
+	}
 });
 
-G.use('post/page', {name: 'comlewod'});
+//G.use('post/page', {name: 'comlewod'});
+
+var re = G.require('post/page');
+//re.bindEvent();
